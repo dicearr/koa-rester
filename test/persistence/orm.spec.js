3980640d5d764ce2cc58890d3eaa05c1f1caaad7
Diego Ceresuela <dcerearr@gmail.com>
2017-03-18 13:54:04 +0100

    ORM2 test added for non-existent table request

--- test/persistence/orm.spec.js
+++ test/persistence/orm.spec.js
@@ -6,9 +6,10 @@ const Koa = require('koa');
 const request = require('supertest');
 
 /**
- * @return {[type]} [description]
+ * @param {boolean} withSync If the table has to be created.
+ * @return {Promise} Resolved with the node orm2 model or rejected with an error
  */
-function wrapper() {
+function wrapper(withSync = true) {
   return new Promise((resolve, reject) => {
     orm.connect('mysql://root@localhost:3306/test', (err, db) => {
       if (err) reject(err);
@@ -16,27 +17,43 @@ function wrapper() {
         firstName: String,
         lastName: String,
       });
-      db.sync((nErr) => {
-        if (nErr) reject(nErr);
-        resolve(model);
-      });
+      if (withSync) {
+        db.sync((nErr) => {
+          if (nErr) reject(nErr);
+          resolve({ model, db });
+        });
+      } else {
+        resolve({ model, db });
+      }
     });
   });
 }
 
 describe('ORM2 CRUD operations', () => {
   let rester;
-  let model;
   beforeEach(() => {
     rester = new Rester({
       router: new Router(),
     });
   });
-  before(async () => {
-    model = await wrapper();
+  it('GET /test/resource should return 500 if the table does not exists', async () => {
+    const c = await wrapper(false);
+    const r = rester.add(c.model, 'test/resource').getList().router;
+    const server = new Koa()
+      .use(r.routes())
+      .use(r.allowedMethods());
+    request(server.listen())
+      .get('/test/resource')
+      .expect(500)
+      .then((res) => {
+        expect(res.body.status).to.equal(500);
+        expect(res.body.message).to.equal('Internal error');
+        c.db.drop();
+      });
   });
-  it('GET /test/resource should return an empty list', (done) => {
-    const r = rester.add(model, 'test/resource').getList().router;
+  it('GET /test/resource should return an empty list', async () => {
+    const c = await wrapper();
+    const r = rester.add(c.model, 'test/resource').getList().router;
     const server = new Koa()
       .use(r.routes())
       .use(r.allowedMethods());
@@ -46,7 +63,7 @@ describe('ORM2 CRUD operations', () => {
       .then((res) => {
         expect(res.body).to.be.instanceof(Array);
         expect(res.body.length).to.equal(0);
-        done();
+        c.db.drop();
       });
   });
 });
